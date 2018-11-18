d8280545c4949c7c46a87b91a1e3559d3837b3f0
FabiennePhD <45097882+FabiennePhD@users.noreply.github.com>
2018-11-18 12:26:40 +0000

    Add files via upload

---
+++ doglycans.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source activate doglycans
+export cl=12
+
+python /home/chem/mstscj/DOGLYCANS/doglycans/prepreader.py [-Wmdt] -f /home/chem/mstscj/DOGLYCANS/doglycans/dat/glycam06h.itp -p /home/chem/mstscj/DOGLYCANS/doglycans/dat/GLYCAM_06h.prep -s seqfile.seq -c ${cl}HGB.pdb -o ${cl}HGB.itp -- charmm
